35d53e2c3d1bcaa6be68a30158f590a7a7c59d40
sezaite <martyna.sezaite@gmail.com>
2021-06-03 14:30:05 +0300

    folders transfered

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Nav from './components/nav/Nav';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/home/Home';
+import Blogs from './components/blogs/Blogs';
+import Work from './components/work/Work';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/work">
+          <Work />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/blogs">
+          <Blogs />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+function Nav() {
+    useEffect(() => {
+        const burger = document.querySelector(".burger");
+        const nav = document.querySelector("nav");
+        const navLinks = document.querySelectorAll("nav a");
+
+        burger.addEventListener("click", () => {
+            nav.classList.toggle("nav-active");
+
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = ""
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+                }
+            });
+            burger.classList.toggle("x");
+        });
+    }, []);
+
+    return (
+        <header>
+            <div className="burger">
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
+            <div className="logo">
+                <h2>ShepSounds</h2>
+            </div>
+            <nav>
+                <NavLink to="/" className="menu-item home" activeClassName="selected">Home</NavLink>
+                <NavLink to="work" className="menu-item work" activeClassName="selected">Work</NavLink>
+                <NavLink to="about" className="menu-item about" activeClassName="selected">About</NavLink>
+                <NavLink to="blog" className="menu-item news" activeClassName="selected">News</NavLink>
+                <NavLink to="index" className="menu-item contact" activeClassName="selected">Contact</NavLink>
+
+                {/* change into links? */}
+
+                <a href="#" target="_blank" className="fa fa-soundcloud" aria-hidden="true"></a>
+                <a href="#" target="_blank" className="fa fa-instagram" aria-hidden="true"></a>
+                <a href="#" target="_blank" className="fa fa-twitter" aria-hidden="true"></a>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
