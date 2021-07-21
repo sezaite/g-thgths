e977c8a695e26562dd156da38d683e8a85451579
sezaite <martyna.sezaite@gmail.com>
2021-07-22 02:50:42 +0300

    list of blogs

--- src/components/work/ListOfWorks.js
+++ src/components/work/ListOfWorks.js
@@ -2,11 +2,7 @@ import WorkItem from "./WorkItem"
 
 function ListOfWorks({ data, width }) {
     return (
-        <>
-            {data.map((item) => (
-                <WorkItem key={item.id} width={width} img={item.imgLink} titles={item.titles} date={item.date} albumName={item.name} header={item.header} description={item.description} paragraph={item.p} spotify={item.spotifyLink} />
-            ))}
-        </>
+        
     )
 }
 
