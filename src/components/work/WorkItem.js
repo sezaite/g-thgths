5a60a558a0c2ac1b396e73e4a2260cc1e01b6077
sezaite <martyna.sezaite@gmail.com>
2021-07-22 02:06:31 +0300

    read more

--- src/components/work/WorkItem.js
+++ src/components/work/WorkItem.js
@@ -1,13 +1,15 @@
 import LinesEllipsis from 'react-lines-ellipsis';
 import { useState } from 'react';
 import ReadMoreReact from 'read-more-react';
+// import ReactReadMoreReadLess from "react-read-more-read-less";
+//import ReadMoreText from 'read-more-less-react';
 
 function WorkItem({ img, titles, date, albumName, header, description, paragraph, spotify, width }) {
-    // const [fullText, setFulltext] = useState(width < 420 ? false : true);
+    const [fullText, setFulltext] = useState(width < 420 ? false : true);
     return (
         <div className="row">
             <div className="col-4 col-md-4 col-sm-12 work-left">
-                <img src={`/img/${img}`} alt="album-art" />
+                <img src={`img/${img}`} alt="album-art" />
                 <h5>{albumName}</h5>
             </div>
             <div className="col-8 col-md-8 col-sm-12 work-right">
@@ -15,20 +17,9 @@ function WorkItem({ img, titles, date, albumName, header, description, paragraph
                     <h3>{header}</h3>
                     <h5>{date}</h5>
                 </div>
-                <ReadMoreReact text={paragraph} readMoreText="Read more" />
-                {/* <p>{paragraph}</p> */}
-                {/* // <>
-                        //     <LinesEllipsis
-                        //         text={paragraph}
-                        //         maxLine='10'
-                        //         ellipsis='...'
-                        //         trimRight
-                        //         basedOn='words'
-                        //         className="elipsis"
-                        //     />
-                        //     <button className="btn read-more" onClick={() => setFulltext(true)}>Read more</button> */}
-                {/* // </> */}
-
+                {fullText ? <p>{paragraph}</p> : (
+                    <ReadMoreReact text={paragraph} readMoreText="Read more" min="400" ideal="500" max="700" />
+                )}
 
 
                 <iframe src={spotify} frameBorder='0' allowtransparency='true' allow='encrypted-media'></iframe>
