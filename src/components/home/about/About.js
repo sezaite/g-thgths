08b0cb97f17d662aabf4cea04d2783eea7a8934f
sezaite <martyna.sezaite@gmail.com>
2021-07-26 15:39:10 +0300

    about me responsive

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -5,13 +5,8 @@ function About() {
         <section className="container about">
             <div className="row">
                 <h2 className="col-12">About me</h2>
-                <div className="about-right col-3">
-                    <img src=" ./img/portrait.jpg" alt="my_profile_picture" />
-                </div>
-
-                <div className="about-left col-8 col-sm-12">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam omnis aliquam molestiae voluptas esse, odio ab quos dolorum est beatae ad laborum quod explicabo sint laudantium dolores corrupti repudiandae aspernatur, modi incidunt quae. Nobis id quos alias recusandae quo eligendi?</p>
-                </div>
+                <img className="about-right col-5 col-sm-6 col-xsm-12" src="https://sezaite.github.io/g-thgths/img/profile.jpg" alt="my_profile_picture" />
+                <p className="about-left col-7 col-sm-6 col-xsm-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam omnis aliquam molestiae voluptas esse, odio ab quos dolorum est beatae ad laborum quod explicabo sint laudantium dolores corrupti repudiandae aspernatur, modi incidunt quae. Nobis id quos alias recusandae quo eligendi?</p>
             </div>
         </section>
     )
